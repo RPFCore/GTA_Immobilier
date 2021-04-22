5f4049e1e565711b1570db88aaa569c4b66b7a68
RPF Studio <groupe-esp-fr@outlook.fr>
2021-04-22 20:48:29 +0200

    Begging !

---
+++ GTA_Immobilier/config/config.lua
@@ -0,0 +1,32 @@
+
+
+
+--- Config : Dont misc change store position !!!!!
+
+Config = {
+    Locations = {
+        [1] = {
+            ["sPed"] = {
+                ["x"] = -95.72, ["y"] = 1433.78, ["z"] = 13.70, ["h"] = 360.82 ---- you need change x,y,z is for Liberty City V
+            },
+            ["clefs"] = {
+                itemNameStudio = {"Clef"},
+                itemNameAppart = {"Clef_2"},
+                itemNameMaison = {"Clef_3","Clef_4"},
+                prix = {
+                    Clef = 30000, 
+                    Clef_2 = 200000,
+                    Clef_3 = 2000000,
+                    Clef_4 = 15000000
+                },
+                pos = {
+                    Clef = -82.8325,1436.9871,15.60, ---- Destination blip when you buy. you need change x,y,z is for Liberty City V
+                    Clef_2 = -82.8325,1436.9871,15.60, ---- Destination blip when you buy. you need change x,y,z is for Liberty City V
+                    Clef_3 = -82.8325,1436.9871,15.60, ---- Destination blip when you buy. you need change x,y,z is for Liberty City V
+                    Clef_4 =  290.54,248.38,9.07 ---- Destination blip when you buy. you need change x,y,z is for Liberty City V
+                }
+            },
+        },
+
+    },
+}
