4c3a35eb7d1bb9a24cb3322fe86fa4dc72e4c3d7
RPF Studio <groupe-esp-fr@outlook.fr>
2021-04-25 12:23:12 +0200

    Update items.sql

--- GTA_Immobilier/items.sql
+++ GTA_Immobilier/items.sql
@@ -1,3 +1,12 @@
+CREATE TABLE IF NOT EXISTS `user_storage` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `item_name` varchar(255) DEFAULT NULL,
+  `quantity` bigint(20) unsigned NOT NULL DEFAULT 0,
+  `license` varchar(50) NOT NULL DEFAULT '0',
+  UNIQUE KEY `item_id_license` (`item_name`,`license`) USING BTREE,
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 INSERT INTO `items` (`libelle`, `isUsable`, `type`) VALUES
 	('Clef', 1, 30000),
 	('Clef_2', 1, 200000);
